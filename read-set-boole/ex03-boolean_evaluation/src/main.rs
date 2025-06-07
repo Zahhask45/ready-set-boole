a09c70cd02db2ff92e22b766b1223a01f7003223
Zahhask <tomasgaspar95@gmail.com>
2025-06-07 18:55:27 +0100

    Need to see what is the idiomatic way to either parse the formula(&str) or the exercise in itself

---
+++ read-set-boole/ex03-boolean_evaluation/src/main.rs
@@ -0,0 +1,27 @@
+#[derive(Debug, Clone, Default)]
+struct Eval {
+ signals: Vec<char>,
+ numbers: Vec<i8>,
+ result: i8,
+}
+
+
+fn eval_formula(formula: &str) -> bool{
+    let mut eval = Eval{
+        signals: Vec::new(),
+        numbers: Vec::new(),
+        result: 0i8,
+    };
+    
+    while (formuala)
+    
+    if eval.result == 1{
+        true
+    }else{
+        false
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
