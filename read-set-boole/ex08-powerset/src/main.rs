0b9682d4a5899d804fc5f8d00f863ea4ae4dbd03
Zahhask <tomasgaspar95@gmail.com>
2025-07-02 14:57:08 +0100

    done the ex08-powerset I hope

--- read-set-boole/ex08-powerset/src/main.rs
+++ read-set-boole/ex08-powerset/src/main.rs
@@ -1,25 +1,25 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct PowerSet {
     value: i32,
-    position: i32,
+    position: u32,
 }
 
-fn parse(set: Vec<i32>) -> Vec<PowerSet>{
+fn parse(set: &Vec<i32>) -> Vec<PowerSet>{
     let mut power: Vec<PowerSet> = Vec::new();
     let mut i = 0u32;
     let base = 2i32;
 
     for iter in set {
-        power.push(PowerSet { value: iter, position: base.pow(i)});
+        power.push(PowerSet { value: *iter, position: base.pow(i) as u32});
         i += 1;
     }
     power
 }
 
-fn powerset(set: Vec<i32>){
+fn powerset(set: Vec<i32>) -> Vec<Vec<i32>>{
     let amount = set.len();
     let mut position = 0u32;
-    // let power = parse(set);
+    let power = parse(&set);
     let base = 2u32;
     let iterations = base.pow(amount as u32);
     let mut final_set: Vec<Vec<i32>> = vec![];
@@ -29,7 +29,6 @@ fn powerset(set: Vec<i32>){
     for i in 0..iterations {
         let mut new_set: Vec<i32> = vec![];
         if i == 0 { final_set.push(new_set.clone()); continue;}
-        println!("{final_set:?}");
         if i == base.pow(position) {
             new_set.push(set[position as usize]);
             final_set.push(new_set.clone());
@@ -37,13 +36,22 @@ fn powerset(set: Vec<i32>){
             continue;
         }
         // put together various values in a set
-        
-        
-        
+        for iter in power.clone() {
+            
+            if i & iter.position != 0{
+                new_set.push(iter.value);
+            }
+        }
+        final_set.push(new_set.clone());
     }
+    final_set
 }
 
 fn main() {
     let set = vec![1, 2, 3];
-    powerset(set);
+    println!("{:?}", powerset(set));
+    let set = vec![1, 2, 3, 4];
+    println!("{:?}", powerset(set));
+    let set = vec![1, 2, 3, 4, 5];
+    println!("{:?}", powerset(set));
 }
